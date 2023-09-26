c8f139cb45b8637cae20737833a9e505161e550a
Enzo Moraes <enzo.moraes@ericsson.com>
2023-09-26 20:06:42 -0300

    atualizacoes

---
+++ README.md
@@ -0,0 +1,6 @@
+# SANOFI
+Enzo Moraes – RM 87424
+Gabriel Aires – RM 88731
+Matheus Martins – RM 89129
+Mike Almeida - RM 87413
+Patrick Neris – RM 88425
---
+++ scriptform.js
@@ -0,0 +1,41 @@
+let cameraAtiva = false;
+
+document.querySelector('#acessarCamera').addEventListener('click', () => {
+    var video = document.querySelector('video');
+
+    if (cameraAtiva) {
+            // const tracks = video.srcObject.getTracks();
+            // tracks.forEach(track => track.stop());
+        video.srcObject = null;
+        cameraAtiva = false;
+    } else {
+        navigator.mediaDevices.getUserMedia({ video: { width: 720, aspectRatio: 21/9, height: 300 } })
+        .then(stream => {
+            video.srcObject = stream;
+            video.play();
+            cameraAtiva = true;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+});
+
+document.querySelector('#tirarFoto').addEventListener('click', () => {
+    var canvas = document.querySelector('canvas');
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0);
+
+    var link = document.createElement('a');
+    link.download = 'foto.png';
+    link.href = canvas.toDataURL();
+    link.textContent = 'Clique para baixar a imagem';
+
+    // Remover elementos antigos, se existirem
+    var existingLink = document.querySelector('a');
+    if (existingLink) {
+        existingLink.remove();
+    }
+
+    document.body.appendChild(link);
+});
